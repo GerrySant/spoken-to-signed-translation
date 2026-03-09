352d86e73d2d94a0bd7af2a62220b36ee91d3b73
Gerard Sant <gerard.sant.muniesa@gmail.com>
2026-03-09 11:38:45 +0100

    [FIX] Add gloss_placeholder param to FingerspellingPoseLookup.lookup()

--- spoken_to_signed/gloss_to_pose/lookup/fingerspelling_lookup.py
+++ spoken_to_signed/gloss_to_pose/lookup/fingerspelling_lookup.py
@@ -46,7 +46,8 @@ class FingerspellingPoseLookup(CSVPoseLookup):
         return pose
 
     def lookup(
-        self, word: str, gloss: str, spoken_language: str, signed_language: str, source: str = None
+        self, word: str, gloss: str, spoken_language: str, signed_language: str, source: str = None,
+        gloss_placeholder: str = None
     ) -> LookupResult:
         if spoken_language not in self.words_index or signed_language not in self.words_index[spoken_language]:
             raise FileNotFoundError(
